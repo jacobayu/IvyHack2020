f34ea44874a73d59b66fcf5b79318b26315762c3
Jacob Au <jacobau.inbox@gmail.com>
2020-10-03 10:03:30 -0700

    Initial X-Code Commit

---
+++ IvyHack2020/IvyHack2020/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IvyHack2020
+//
+//  Created by Jacob Au on 10/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
