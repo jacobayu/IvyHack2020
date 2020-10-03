9c5166cf7a729e8bc01a87cbbf43e96f8229c3ee
Jacob Au <jacobau.inbox@gmail.com>
2020-10-03 15:15:00 -0700

    revisedStoryBoardPostPush

--- NewIvyHackProject/NewIvyHackProject/ViewController.swift
+++ NewIvyHackProject/NewIvyHackProject/ViewController.swift
@@ -9,44 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
-<<<<<<< HEAD
-
-
-    
-
-
-
-
-    @IBOutlet weak var inputName: UITextField!
-
-    @IBOutlet weak var outputName: UILabel!
-
-
-    @IBAction func changeLabel(_ sender: Any) {
-        outputName.text = inputName.text
-    }
-
->>>>>>> dff7bbcee9b98bf6215c9a812ce871c68690a725
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-<<<<<<< HEAD
-
-
-
-    @IBAction func homeToMessageButton(_ sender: Any) {
-        performSegue(withIdentifier: "homeToMessagesSegue", sender: self)
-
-    }
-
-
-    @IBAction func homeToDiscoverButton(_ sender: Any) {
-        performSegue(withIdentifier: "homeToDiscoverSegue", sender: self)
-
-    }
->>>>>>> dff7bbcee9b98bf6215c9a812ce871c68690a725
 
 
 
